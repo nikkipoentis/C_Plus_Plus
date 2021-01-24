d1a61d7f0bfa6ad4bf95ea766c80eb52ffa11c98
nikkipoentis <poentis@chapman.edu>
2021-01-23 14:14:49 -1000

    first commit

---
+++ problem_cereal.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+  const double OZ_TO_TON = 35273.92;
+  int weightOz;
+  double weightTon;
+  int numBoxes;
+
+  cout << "Cereal box weight in ounces: ";
+  cin >> weightOz;
+
+  weightTon = weightOz / OZ_TO_TON;
+  numBoxes = OZ_TO_TON / weightOz;
+  if ((int) OZ_TO_TON % weightOz != 0)
+  {
+    ++numBoxes;
+  }
+
+  cout << "This box of cereal weights " << weightTon << " metric tons.\nIt would take " << numBoxes
+  << " boxes to fill a one ton box." << endl;
+
+  return 0;
+}
