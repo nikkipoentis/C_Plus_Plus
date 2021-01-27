b5372ea4caa1bd0ff02992dd3eb152ebc73cb30f
nikkipoentis <poentis@chapman.edu>
2021-01-27 11:46:20 -1000

    Assignment 1

---
+++ Assignment_1/problem_firecapacity.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main (int argc, char **argv)
+{
+  int maxPeople;
+  int numPeople;
+
+  cout << "Maximum room capacity: ";
+  cin >> maxPeople;
+
+  cout << "Number of people attending: ";
+  cin >> numPeople;
+
+  if (numPeople <= maxPeople)
+  {
+    cout << "It is legal to hold this meeting. " << maxPeople - numPeople << " more person(s) may attend.";
+  }
+  else
+  {
+    cout << "This meeting cannot be held as planned due to fire regulations. The attendance is "
+      << numPeople - maxPeople << " person(s) above the room's maximum capacity.";
+  }
+
+  cout << endl;
+
+  return 0;
+}
